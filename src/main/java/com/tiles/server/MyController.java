24c6ebcd088f51fbf0ab1d9893957cf4374d4ed9
danielshek <daniel.shekhtman@students.mq.edu.au>
2026-05-27 15:12:29 +0000

    Reorganised imports

--- src/main/java/com/tiles/server/MyController.java
+++ src/main/java/com/tiles/server/MyController.java
@@ -5,14 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import io.micrometer.common.util.StringUtils;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.http.ResponseEntity;
-
 import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 @RestController
